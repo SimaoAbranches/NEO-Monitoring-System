b90266ef9b4ef7ee19e03c3760c5f84233f06a63
SimaoAbranches <simaoabranches6@gmail.com>
2025-12-18 16:27:40 +0000

    Update DCL_Permissions.sql

--- database/DCL_Permissions.sql
+++ database/DCL_Permissions.sql
@@ -1,23 +1,21 @@
 USE [NEO_Monitoring_DB];
 GO
 
--- 1. Criar um Login para a Aplicação Python (Nível de Servidor)
--- Nota: 'AppPassword123' deve ser trocada por uma segura
+-- Criar um Login para a Aplicação Python (Nível de Servidor)
 IF NOT EXISTS (SELECT * FROM sys.server_principals WHERE name = 'NEO_App_User')
 BEGIN
     CREATE LOGIN NEO_App_User WITH PASSWORD = 'AppPassword123';
 END
 GO
 
--- 2. Criar um Utilizador na Base de Dados para esse Login
+-- Criar um Utilizador na Base de Dados para esse Login
 IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = 'NEO_App_User')
 BEGIN
     CREATE USER NEO_App_User FOR LOGIN NEO_App_User;
 END
 GO
 
--- 3. Definir Permissões (DCL - Data Control Language)
--- O utilizador da App só pode ler dados e executar procedimentos
+-- Definir Permissões (DCL - Data Control Language)
 GRANT SELECT ON SCHEMA::dbo TO NEO_App_User;
 GRANT EXECUTE ON SCHEMA::dbo TO NEO_App_User;
 
@@ -25,7 +23,6 @@ GRANT EXECUTE ON SCHEMA::dbo TO NEO_App_User;
 DENY ALTER ON SCHEMA::dbo TO NEO_App_User;
 GO
 
--- 4. Criar um Papel (Role) para os Colegas de Grupo (Administradores)
 IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = 'Group_Admins' AND type = 'R')
 BEGIN
     CREATE ROLE Group_Admins;
