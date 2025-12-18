d2b0d69c29d28bfff5bf1b14f00fd3342b2544da
SimaoAbranches <simaoabranches6@gmail.com>
2025-12-18 09:00:01 +0000

    Update Triggers_Alerts.sql

--- database/Triggers_Alerts.sql
+++ database/Triggers_Alerts.sql
@@ -1,7 +1,7 @@
 USE [NEO_Monitoring_DB];
 GO
 
--- Impede que se insiram observa��es com tempo negativo ou zero
+-- Impede que se insiram observações com tempo negativo ou zero
 CREATE OR ALTER TRIGGER trg_ValidateObservationDuration
 ON Observation
 AFTER INSERT, UPDATE
@@ -9,8 +9,9 @@ AS
 BEGIN
     IF EXISTS (SELECT 1 FROM inserted WHERE duration_minutes <= 0)
     BEGIN
-        RAISERROR ('Erro: A dura��o da observa��o deve ser positiva.', 16, 1);
+        RAISERROR ('Erro: A duração da observação deve ser positiva.', 16, 1);
         ROLLBACK TRANSACTION;
     END
 END;
 GO
+
