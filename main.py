f488223a195641e655c195df897096c067d94572
SimaoAbranches <simaoabranches6@gmail.com>
2025-12-18 16:05:02 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import pandas as pd
+from sqlalchemy import create_engine
+import urllib
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+params = urllib.parse.quote_plus(
+    r'DRIVER={ODBC Driver 17 for SQL Server};SERVER=LAPTOP-E1984K43\SQLEXPRESS;DATABASE=NEO_Monitoring_DB;Trusted_Connection=yes;')
+engine = create_engine(f"mssql+pyodbc:///?odbc_connect={params}")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+csv_path = 'Data/neo.csv'
+try:
+    print("A ler o ficheiro CSV...")
+
+    colunas_csv = ['spkid', 'name', 'full_name', 'diameter']
+
+    df = pd.read_csv(csv_path, sep=';', low_memory=False, usecols=colunas_csv)
+
+    df = df.replace('<null>', None)
+
+
+    df['full_name'] = df['full_name'].fillna(df['name']).fillna(df['spkid'].astype(str))
+
+
+    df['name'] = df['name'].fillna('Unnamed')
+
+    print(f"A iniciar a importação de {len(df)} linhas...")
+
+
+    df.to_sql('Asteroid', con=engine, if_exists='append', index=False, chunksize=500)
+
+    print("Sucesso! Os dados foram finalmente importados.")
+
+except Exception as e:
+    print(f"Ocorreu um erro: {e}")
