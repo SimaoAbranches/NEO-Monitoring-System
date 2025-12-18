bee30ad60d68c646aaaa5c1baae56a6bfe3d524b
SimaoAbranches <simaoabranches6@gmail.com>
2025-12-18 16:28:42 +0000

    Update Indexes_Optimization.sql

--- database/Indexes_Optimization.sql
+++ database/Indexes_Optimization.sql
@@ -1,27 +1,22 @@
 USE [NEO_Monitoring_DB];
 GO
 
--- 1. Índice para Pesquisa por Nome
--- Essencial porque os utilizadores vão pesquisar asteroides pelo nome no Python
+-- Índice para Pesquisa por Nome
 CREATE INDEX IX_Asteroid_Name 
 ON Asteroid (name);
 GO
 
--- 2. Índice para o Diímetro
--- Como a tua View de estatísticas calcula médias e máximos, este índice acelera o processo
+-- Índice para o Diímetro
 CREATE INDEX IX_Asteroid_Diameter 
 ON Asteroid (diameter);
 GO
 
--- 3. Índice Composto para Observações
--- Acelera a filtragem de quem observou o quê e em que data
+-- Índice Composto para Observações
 CREATE INDEX IX_Observation_Astronomer_Date
 ON Observation (astronomer_id, observation_date);
 GO
 
--- 4. Índice para Chaves Estrangeiras
--- O SQL Server não cria automaticamente Índices para FKs. 
--- Isto acelera a View_Astronomer_Observation_Summary que criaste
+-- Índice para Chaves Estrangeiras
 CREATE INDEX IX_Observation_AsteroidID
 ON Observation (asteroid_id);
 GO
